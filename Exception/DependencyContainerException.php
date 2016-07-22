eca1ff9cd5b3c0a7ab530f0e978485e3e3072d76
Ryan M. Poe <rmp23@duke.edu>
2016-07-22 13:01:00 +0000

    Initial commit

---
+++ Exception/DependencyContainerException.php
@@ -0,0 +1,18 @@
+<?php
+
+class DependencyContainerException implements Psr\Container\Exception\ContainerExceptionInterface
+{
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    // custom string representation of object
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+
+    public function customFunction() {
+        echo "A custom function for this type of exception\n";
+    }
+}
---
+++ Test/test.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once("../DependencyContainer.php");
+
+$container = new DependencyContainer();
+
+$global_scope_checker = true;
+
+$container->inject("test", function($arguments) {
+    return  [
+        "depends" =>
+          $arguments["sammy"] . $arguments[0] . $arguments[1]()  
+        ];
+    },
+    [   "sammy" => "zane",
+        3,
+        function() { return 42; }
+    ]
+);
+
+$container->inject("test2", function($args) {
+    global $global_scope_checker;
+    $global_scope_checker = false;
+    return true;
+});
+
+
+echo "Beginning test for DependencyContainer\n\n";
+
+assert(!$container->has("test"));
+echo ".";
+
+$dep = $container->get("test");
+assert(is_array($dep));
+echo ".";
+
+assert(count($dep) == 1);
+echo ".";
+
+assert("zane342" == $dep["depends"]);
+echo ".";
+
+/**
+ * This test ensures that dependencies aren't initialized until they're called,
+ * to ensure that we can setup a bunch of dependencies up front, but not necessarily
+ * need to load them all into memory if our request doesn't call for it.
+ */
+
+assert($global_scope_checker);
+echo ".";
+
+$dep = $container->get("test2");
+assert(!$global_scope_checker);
+echo ".";
+
+assert($dep);
+echo ".";
+
+echo "\n\nTest suite complete! All tests pass.\n";
